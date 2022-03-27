4f83ffa47d2830e71cab518831dc0cf971a3d5bd
Huseyin-Senel <96916049+Huseyin-Senel@users.noreply.github.com>
2022-03-27 18:50:10 +0300

    Add files via upload

---
+++ Regression/AI-Regression.py
@@ -0,0 +1,43 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import random
+
+
+
+"""Regression function"""
+def regression(x, y):
+    """
+    :param x: x-axis
+    :param y: y-axis
+    :return: slope, y-intercept
+    """
+    x_mean = np.mean(x)
+    y_mean = np.mean(y)
+    numerator = 0
+    denominator = 0
+    for i in range(len(x)):
+        numerator += (x[i] - x_mean) * (y[i] - y_mean)
+        denominator += (x[i] - x_mean) ** 2
+    slope = numerator / denominator
+    y_intercept = y_mean - slope * x_mean
+    return slope, y_intercept
+
+
+
+x =  np.array(list(range(1, 100)))
+y =  np.array(list(range(1, 100)))
+for i in range(len(x)):
+    y[i]= x[i]+random.randint(0,50)
+
+
+
+"""plot regression line"""
+slope, y_intercept = regression(x, y)
+x_line = np.linspace(0, 200, 200)
+y_line = slope * x_line + y_intercept
+plt.plot(x_line, y_line, 'r')
+#plt.xlim([0, 200])
+#plt.ylim([0, 200])
+plt.scatter(x, y)
+plt.show()
+
